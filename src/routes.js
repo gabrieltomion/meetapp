2cf45a78ed3e4e84337233ca9ef2f389dbbeb886
Gabriel Tomio Nardes <gabrieltomion@outlook.com>
2019-07-13 12:13:15 -0300

    Criado upload de arquivos

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,17 @@
 import { Router } from 'express';
+import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+
+import multerConfig from './config/multer';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/session', SessionController.store);
 routes.post('/users', UserController.store);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
